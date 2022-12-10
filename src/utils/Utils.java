a3bc6feb59c5d8688b826cb88fc3fef95902f564
PreCyz <precpaw@op.pl>
2022-12-10 19:19:25 +0100

    Solution Dec 9. working part 1.

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -11,4 +11,73 @@ public final class Utils {
         return Paths.get(dir.toString(), fileName).toAbsolutePath().normalize().toString();
     }
 
+    public static String[][] adjustGrid(String[][] grid, int x, int y) {
+        int newX = grid.length;
+        int newY = grid[0].length;
+        if (x < 0) {
+            newX += Math.abs(x);
+        } else if (x >= grid.length) {
+            newX += x - grid.length;
+        }
+        if (y < 0) {
+            newY += Math.abs(y);
+        } else if (y >= grid[0].length) {
+            newY += y - grid[0].length;
+        }
+
+        String[][] adjustedGrid = new String[newX][newY];
+        for (int yy = 0; yy < newY; yy++) {
+            for (int xx = 0; xx < newX; xx++) {
+                adjustedGrid[xx][yy] = ".";
+            }
+        }
+
+        for (int yy = 0; yy < grid[0].length; yy++) {
+            for (int xx = 0; xx < grid.length; xx++) {
+                newX = xx;
+                newY = yy;
+                if (x < 0) {
+                    newX = Math.abs(xx - x);
+                }
+                if (y < 0) {
+                    newY = Math.abs(yy - y);
+                }
+                adjustedGrid[newX][newY] = grid[xx][yy];
+            }
+        }
+        return adjustedGrid;
+    }
+
+    public static void main(String[] args) {
+        String[][] grid = new String [][] {
+                {"3", "1"},
+                {"4", "2"}
+        };
+        printGrid2(grid);
+
+        System.out.println("After adjustment");
+        String[][] strings = adjustGrid(grid, -1, -1);
+        printGrid2(strings);
+        System.out.println("After adjustment");
+        strings = adjustGrid(strings, 3, 3);
+        printGrid2(strings);
+        System.out.println("After adjustment");
+        strings = adjustGrid(strings, 4, 4);
+        printGrid2(strings);
+
+        System.out.println("After adjustment");
+        strings = adjustGrid(strings, -2, 5);
+        printGrid2(strings);
+
+    }
+
+    static void printGrid2(String[][] grid) {
+        for (int y = grid[0].length - 1; y >= 0; y--) {
+            for (int x = 0; x < grid.length; x++) {
+                System.out.printf(grid[x][y]);
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/year2022/Dec9.java
+++ src/year2022/Dec9.java
@@ -1,12 +1,14 @@
 package year2022;
 
 import base.DecBase;
+import utils.Utils;
 
 import java.util.*;
 import java.util.stream.Stream;
 
 class Dec9 extends DecBase {
     private static final String TAIL = "T";
+    private static final String HEAD = "H";
 
     protected Dec9(String fileName) {
         super(fileName);
@@ -30,6 +32,12 @@ class Dec9 extends DecBase {
 
     @Override
     protected void calculatePart1() {
+        calculate(2, "Part 1");
+    }
+
+    void calculate(int knotSize, String part) {
+        Map<String, Knot> visitedMap = new LinkedHashMap<>();
+
         int yLength = inputStrings.stream()
                 .filter(l -> l.startsWith("U"))
                 .mapToInt(u -> Integer.parseInt(u.split(" ")[1]))
@@ -39,74 +47,77 @@ class Dec9 extends DecBase {
                 .mapToInt(r -> Integer.parseInt(r.split(" ")[1]))
                 .sum();
 
-        int gridLength = 2 * Math.max(xLength, yLength);
+        int gridLength = Math.max(xLength, yLength);
         String[][] grid = new String[gridLength][gridLength];
         initGrid(grid, gridLength);
-        int headX = gridLength / 2;
-        int headY = gridLength / 2;
-        int tailX = gridLength / 2;
-        int tailY = gridLength / 2;
-        grid[tailX][tailY] = TAIL;
+
+        ArrayList<Knot> knots = new ArrayList<>(knotSize);
+        for (int idx = 0; idx < knotSize; idx++) {
+            if (idx == 0) {
+                knots.add(idx, new Knot(gridLength / 2, gridLength / 2, HEAD));
+            } else if (idx == knotSize - 1) {
+                knots.add(idx, new Knot(gridLength / 2, gridLength / 2, TAIL));
+            } else {
+                knots.add(idx, new Knot(gridLength / 2, gridLength / 2, String.valueOf(idx)));
+            }
+        }
+
+        grid[gridLength / 2][gridLength / 2] = TAIL;
+        Knot lastTail = knots.get(knots.size() - 1);
+        visitedMap.put(lastTail.key(), lastTail);
 
         for (String line : inputStrings) {
             String[] split = line.split(" ");
             String direction = split[0];
             int steps = Integer.parseInt(split[1]);
 
-            for (int i = 1; i <= steps; i++) {
+            Knot head = knots.get(0);
+            for (int step = 1; step <= steps; step++) {
+
                 switch (direction) {
-                    case "U" -> {
-                        headY += 1;
-                        if (Math.abs(headY - tailY) > 1) {
-                            if (Math.abs(headX - tailX) > 0) {
-                                //System.out.printf("U pull before tailX=%d, and after headX=%d %n", tailX, headX);
-                                tailX = headX;
-                            }
-                            tailY += 1;
-                            grid[tailX][tailY] = TAIL;
-                        }
-                    }
-                    case "D" -> {
-                        headY -= 1;
-                        if (Math.abs(headY - tailY) > 1) {
-                            if (Math.abs(headX - tailX) > 0) {
-                                //System.out.printf("D pull before tailX=%d, and after headX=%d %n", tailX, headX);
-                                tailX = headX;
-                            }
-                            tailY -= 1;
-                            grid[tailX][tailY] = TAIL;
-                        }
-                    }
-                    case "R" -> {
-                        headX += 1;
-                        if (Math.abs(headX - tailX) > 1) {
-                            if (Math.abs(headY - tailY) > 0) {
-                                //System.out.printf("R pull before tailY=%d, and after headY=%d %n", tailY, headY);
-                                tailY = headY;
-                            }
-                            tailX += 1;
-                            grid[tailX][tailY] = TAIL;
-                        }
-                    }
-                    case "L" -> {
-                        headX -= 1;
-                        if (Math.abs(headX - tailX) > 1) {
-                            if (Math.abs(headY - tailY) > 0) {
-                                //System.out.printf("L pull before tailY=%d, and after headY=%d %n", tailY, headY);
-                                tailY = headY;
-                            }
-                            tailX -= 1;
-                            grid[tailX][tailY] = TAIL;
-                        }
-                    }
+                    case "U" -> head.y += 1;
+                    case "D" -> head.y -= 1;
+                    case "R" -> head.x += 1;
+                    case "L" -> head.x -= 1;
                     default -> throw new IllegalArgumentException(direction);
                 }
-                //System.out.printf("H[%d,%d] T[%d,%d]%n", headX, headY, tailX, tailY);
-            }
-        }
+                updateGrid(grid, head.x, head.y, "H");
 
-        //printGrid(grid, gridLength);
-        System.out.printf("Part 1: %d%n", calculateTales(grid, gridLength, TAIL));
+                Knot tmpHead = head;
+                for (int knotNo = 1; knotNo < knots.size(); knotNo++) {
+                    Knot tail = knots.get(knotNo);
+                    //diagonal move y-wise
+                    if (Math.abs(tmpHead.y - tail.y) > 1 && Math.abs(tmpHead.x - tail.x) > 0) {
+                        tail.y += tmpHead.y - tail.y + (tmpHead.y > tail.y ? -1 : 1);
+                        tail.x = Math.abs(tmpHead.x - tail.x) > 1 ? tmpHead.x - 1 : tmpHead.x;
+                        //diagonal move x-wise
+                    } else if (Math.abs(tmpHead.x - tail.x) > 1 && Math.abs(tmpHead.y - tail.y) > 0) {
+                        tail.x += tmpHead.x - tail.x + (tmpHead.x > tail.x ? -1 : 1);
+                        tail.y = Math.abs(tmpHead.y - tail.y) > 1 ? tmpHead.y - 1 : tmpHead.y;
+                        // move only y-wise
+                    } else if (Math.abs(tmpHead.y - tail.y) > 1) {
+                        tail.y += tmpHead.y - tail.y + (tmpHead.y > tail.y ? -1 : 1);
+                        //move only x-wise
+                    } else if (Math.abs(tmpHead.x - tail.x) > 1) {
+                        tail.x += tmpHead.x - tail.x + (tmpHead.x > tail.x ? -1 : 1);
+                    }
+                    updateGrid(grid, tail.x, tail.y, String.valueOf(tail.id));
+                    tmpHead = tail;
+                }
+                visitedMap.put(tmpHead.key(), tmpHead);
+                printGrid2(grid);
+                printGrid(grid);
+            }
+            //System.out.println(line + " step " + step + " knot " + knotNo);
+            //System.out.println(line);
+            //print3(gridLength, knots);
+            //printGrid(grid, gridLength);
+        }
+        //printGrid2(grid);
+        //System.out.println();
+        //printGrid(grid);
+        System.out.printf("%s: grid %d%n", part, calculateTales(grid, gridLength));
+        System.out.printf("%s: key size %d%n", part, visitedMap.size());
     }
 
     void initGrid(String[][] grid, int length) {
@@ -117,36 +128,35 @@ class Dec9 extends DecBase {
         }
     }
 
-    void printGrid(String[][] grid, int length) {
-        for (int y = length - 1; y >= 0; y--) {
-            for (int x = 0; x < length; x++) {
-                System.out.printf("9".equals(grid[x][y]) ? "x" : ".");
-                //System.out.printf(!"".equals(grid[x][y]) ? grid[x][y] : ".");
+    void printGrid(String[][] grid) {
+        for (int y = grid[0].length - 1; y >= 0; y--) {
+            for (int x = 0; x < grid.length; x++) {
+                System.out.printf(TAIL.equals(grid[x][y]) ? "x" : ".");
             }
             System.out.println();
         }
     }
 
-    long calculateTales(String[][] grid, int length, String value) {
+    long calculateTales(String[][] grid, int length) {
         long positions = 0;
         for (int y = 0; y < length; y++) {
             for (int x = 0; x < length; x++) {
-                positions += value.equals(grid[x][y]) ? 1 : 0;
+                positions += Dec9.TAIL.equals(grid[x][y]) ? 1 : 0;
             }
         }
         return positions;
     }
 
     private static class Knot {
-        Knot(int x, int y, String no) {
+        Knot(int x, int y, String id) {
             this.x = x;
             this.y = y;
-            this.no = no;
+            this.id = id;
         }
 
         int x;
         int y;
-        String no;
+        String id;
 
         public String key() {
             return x + "," + y;
@@ -166,7 +176,7 @@ class Dec9 extends DecBase {
 //                "L 5",
 //                "R 2"
 //        ).toList());
-
+//
 //        inputStrings = new LinkedList<>(Stream.of(
 //                "R 5",
 //                "U 8",
@@ -178,99 +188,7 @@ class Dec9 extends DecBase {
 //                "U 20"
 //        ).toList());
 
-        Set<String> visited = new LinkedHashSet<>();
-        Map<String, Knot> visitedMap = new LinkedHashMap<>();
-
-        int yLength = inputStrings.stream()
-                .filter(l -> l.startsWith("U"))
-                .mapToInt(u -> Integer.parseInt(u.split(" ")[1]))
-                .sum();
-        int xLength = inputStrings.stream()
-                .filter(l -> l.startsWith("R"))
-                .mapToInt(r -> Integer.parseInt(r.split(" ")[1]))
-                .sum();
-
-        int gridLength = 2 * Math.max(xLength, yLength);
-        String[][] grid = new String[gridLength][gridLength];
-        initGrid(grid, gridLength);
-
-        final int knotSize = 10;
-        ArrayList<Knot> knots = new ArrayList<>(knotSize);
-        for (int idx = 0; idx < knotSize; idx++) {
-            knots.add(idx, new Knot(gridLength / 2, gridLength / 2, idx == 0 ? "H" : String.valueOf(idx)));
-        }
-
-        grid[gridLength / 2][gridLength / 2] = String.valueOf(knotSize - 1);
-        Knot lastTail = knots.get(knots.size() - 1);
-        visited.add(lastTail.key());
-        visitedMap.put(lastTail.key(), lastTail);
-
-        for (String line : inputStrings) {
-            //System.out.printf("%n== %s ==%n", line);
-            String[] split = line.split(" ");
-            String direction = split[0];
-            int steps = Integer.parseInt(split[1]);
-
-            Knot head = knots.get(0);
-            for (int step = 1; step <= steps; step++) {
-                //System.out.printf("%n== %s ==, step: %d%n", line, step);
-
-                switch (direction) {
-                    case "U" -> head.y += 1;
-                    case "D" -> head.y -= 1;
-                    case "R" -> head.x += 1;
-                    case "L" -> head.x -= 1;
-                    default -> throw new IllegalArgumentException(direction);
-                }
-                updateGrid(grid, head.x, head.y, "H");
-
-                Knot tmpHead = head;
-                for (int knotNo = 1; knotNo < knots.size(); knotNo++) {
-                    Knot tail = knots.get(knotNo);
-                    //diagonal move y-wise
-                    if (Math.abs(tmpHead.y - tail.y) > 1 && Math.abs(tmpHead.x - tail.x) > 0) {
-                        tail.y += tmpHead.y - tail.y + (tmpHead.y > tail.y ? -1 : 1);
-                        tail.x = Math.abs(tmpHead.x - tail.x) > 1 ? tmpHead.x - 1 : tmpHead.x;
-                    //diagonal move x-wise
-                    } else if (Math.abs(tmpHead.x - tail.x) > 1 && Math.abs(tmpHead.y - tail.y) > 0) {
-                        tail.x += tmpHead.x - tail.x + (tmpHead.x > tail.x ? -1 : 1);
-                        tail.y = Math.abs(tmpHead.y - tail.y) > 1 ? tmpHead.y - 1 : tmpHead.y;
-                    // move only y-wise
-                    } else if (Math.abs(tmpHead.y - tail.y) > 1) {
-                        tail.y += tmpHead.y - tail.y + (tmpHead.y > tail.y ? -1 : 1);
-                    //move only x-wise
-                    } else if (Math.abs(tmpHead.x - tail.x) > 1) {
-                        tail.x += tmpHead.x - tail.x + (tmpHead.x > tail.x ? -1 : 1);
-                    }
-                    updateGrid(grid, tail.x, tail.y, String.valueOf(knotNo));
-                    tmpHead = tail;
-                }
-                if ("9".equals(tmpHead.no)) {
-                    visited.add(tmpHead.key());
-                    visitedMap.put(tmpHead.key(), tmpHead);
-                }
-                //printGrid2(grid, gridLength);
-                //printGrid(grid, gridLength);
-            }
-            //System.out.println(line + " step " + step + " knot " + knotNo);
-            //print3(gridLength, knots);
-            //System.out.println(line);
-            //printGrid(grid, gridLength);
-
-//            initGrid(grid, gridLength);
-//            grid[gridLength / 2][gridLength / 2] = "s";
-//            for (Knot knot : knots) {
-//                grid[knot.x][knot.y] = knot.no;
-//            }
-//            printGrid2(grid, gridLength);
-        }
-        //printGrid2(grid, gridLength);
-        //System.out.println();
-        //printGrid(grid, gridLength);
-        System.out.printf("Part 2: grid %d%n", calculateTales(grid, gridLength, "" + (knotSize - 1)));
-        System.out.printf("Part 2: set size %d%n", visited.size());
-        System.out.printf("Part 2: key size %d%n", visitedMap.size());
-        //printGrid2(grid, gridLength);
+        calculate(10, "Part 2");
     }
 
     private void print3(int gridLength, ArrayList<Knot> knots) {
@@ -278,20 +196,21 @@ class Dec9 extends DecBase {
         initGrid(tmpGrid, gridLength);
         tmpGrid[gridLength / 2][gridLength / 2] = "s";
         for (Knot knot : knots) {
-            tmpGrid[knot.x][knot.y] = knot.no;
+            tmpGrid[knot.x][knot.y] = knot.id;
         }
-        printGrid2(tmpGrid, gridLength);
+        printGrid2(tmpGrid);
     }
 
     void updateGrid(String[][] grid, int x, int y, String value) {
-        if (!"9".equals(grid[x][y])) {
+        grid = Utils.adjustGrid(grid, x, y);
+        if (!TAIL.equals(grid[x][y])) {
             grid[x][y] = value;
         }
     }
 
-    void printGrid2(String[][] grid, int length) {
-        for (int y = length - 1; y >= 0; y--) {
-            for (int x = 0; x < length; x++) {
+    void printGrid2(String[][] grid) {
+        for (int y = grid[0].length - 1; y >= 0; y--) {
+            for (int x = 0; x < grid.length; x++) {
                 System.out.printf(grid[x][y]);
             }
             System.out.println();
