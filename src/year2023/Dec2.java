5c7c82ebd963c2a08f513548ac3f7572a6dc2b12
Precyzik <gawaprecyz@gmail.com>
2023-12-02 21:28:14 +0100

    2023 dec2.

---
+++ src/year2023/Dec2.java
@@ -0,0 +1,97 @@
+package year2023;
+
+import base.DecBase;
+
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+class Dec2 extends DecBase {
+
+    final int blue = 14;
+    final int red = 12;
+    final int green = 13;
+    final String greenTxt = "green";
+    final String blueTxt = "blue";
+    final String redTxt = "red";
+    final String gameTxt = "Game";
+    public Dec2(String fileName) {
+        super(fileName);
+    }
+
+    @Override
+    public Dec2 readDefaultInput() {
+        System.out.println("Reading default input.");
+        inputStrings = new LinkedList<>(Stream.of(
+                        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
+                .toList());
+        return this;
+    }
+
+    @Override
+    protected void calculatePart1() {
+        long sum = 0;
+        for (String line : inputStrings) {
+            String gameId = line.split(":")[0];
+            String ballsSet = line.split(":")[1];
+            String[] game = ballsSet.split(";");
+            boolean isAllowed = true;
+            for (String round : game) {
+                String[] balls = round.split(",");
+                for (String ball : balls) {
+                    if (ball.contains(blueTxt)) {
+                        isAllowed &= isAllowed(ball, blueTxt, blue);
+                    } else if (ball.contains(redTxt)) {
+                        isAllowed &= isAllowed(ball, redTxt, red);
+                    } else if (ball.contains(greenTxt)) {
+                        isAllowed &= isAllowed(ball, greenTxt, green);
+                    }
+                }
+            }
+            if (isAllowed) {
+                String id = gameId.replace(gameTxt, "").replaceAll(" ", "");
+                sum += Integer.parseInt(id);
+            }
+
+        }
+        System.out.printf("Part 1 - Total score %d%n", sum);
+    }
+
+    private boolean isAllowed(String ball, String text, int max) {
+        String number = ball.replace(text, "").replaceAll(" ", "");
+        int no = Integer.parseInt(number);
+        return no <= max;
+    }
+
+    @Override
+    protected void calculatePart2() {
+        int sum = 0;
+        for (String line : inputStrings) {
+            String ballsSet = line.split(":")[1];
+            String[] game = ballsSet.split(";");
+            int maxGreen = 0, maxRed = 0, maxBlue = 0;
+            for (String round : game) {
+                String[] balls = round.split(",");
+                for (String ball : balls) {
+                    if (ball.contains(blueTxt)) {
+                        maxBlue = getMaxValue(ball, blueTxt, maxBlue);
+                    } else if (ball.contains(redTxt)) {
+                        maxRed = getMaxValue(ball, redTxt, maxRed);
+                    } else if (ball.contains(greenTxt)) {
+                        maxGreen = getMaxValue(ball, greenTxt, maxGreen);
+                    }
+                }
+            }
+            sum += maxRed * maxGreen * maxBlue;
+        }
+        System.out.printf("Part 2 - Total score %d%n", sum);
+    }
+
+    private int getMaxValue(String ball, String text, int max) {
+        String number = ball.replace(text, "").replaceAll(" ", "");
+        return Math.max(Integer.parseInt(number), max);
+    }
+}
---
+++ src/year2023/Main2023.java
@@ -0,0 +1,45 @@
+package year2023;
+
+import base.DecBase;
+import utils.Utils;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main2023 {
+
+    public static void main(String[] args) {
+        Path inputsDir = Paths.get("", "inputs", "2023");
+        if (args.length == 0) {
+            System.out.printf("Args taken from the launch arguments: %s%n", Arrays.asList(args));
+            inputsDir = Paths.get("", args);
+        }
+
+        List<DecBase> adventTasks = List.of(
+//                new Dec1(Utils.getFilePath(inputsDir, "dec_1.txt")),
+                new Dec2(Utils.getFilePath(inputsDir, "dec_2.txt"))
+//                new Dec3(Utils.getFilePath(inputsDir, "dec_3.txt")),
+//                new Dec4(Utils.getFilePath(inputsDir, "dec_4.txt")),
+//                new Dec5(Utils.getFilePath(inputsDir, "dec_5.txt")),
+//                new Dec6(Utils.getFilePath(inputsDir, "dec_6.txt")),
+//                new Dec7(Utils.getFilePath(inputsDir, "dec_7.txt")),
+//                new Dec8(Utils.getFilePath(inputsDir, "dec_8.txt")),
+//                new Dec9(Utils.getFilePath(inputsDir, "dec_9.txt")),
+//                new Dec10(Utils.getFilePath(inputsDir, "dec_10.txt")),
+//                new Dec11(Utils.getFilePath(inputsDir, "dec_11.txt")),
+//                new Dec12(Utils.getFilePath(inputsDir, "dec_12.txt")),
+//                new Dec13(Utils.getFilePath(inputsDir, "dec_13.txt")),
+//                new Dec14(Utils.getFilePath(inputsDir, "dec_14.txt")),
+//                new Dec15(Utils.getFilePath(inputsDir, "dec_15.txt")),
+//                new Dec16(Utils.getFilePath(inputsDir, "dec_16.txt")),
+//                new Dec19(Utils.getFilePath(inputsDir, "dec_19.txt")),
+//                new Dec20(Utils.getFilePath(inputsDir, "dec_20.txt"))
+
+        );
+
+        DecBase.runTasks(adventTasks);
+    }
+}
+
